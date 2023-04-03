ead8278d51f1fedb6641a688e35acff850c268ae
Alexander Kosyakov <110721351+gOweRkrd@users.noreply.github.com>
2023-04-03 12:51:40 +0700

    Add files via upload

---
+++ FoodDeliveryFastFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDeliveryFastFood
+//
+//  Created by Александр Косяков on 03.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
